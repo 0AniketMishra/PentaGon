d7fc0c2911077e0b5086c7a06059abd2bf53cf85
ConnectionsLLC <0aniketmishra@gmail.com>
2022-09-09 16:40:26 +0530

    Resized Stories.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1614aa68913b4278"; }
+/******/ 		__webpack_require__.h = function() { return "b191990429a4a09d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,9 +4,9 @@ import Stories from './Stories'
 function Feed() {
   return (
     
-   <main className='grid grid-cols-1 md:grid-cols-1 xl:grid-cols-3xl xl:grid-cols-1 xl:max-w-6xl mx-auto'> 
+   <main className='grid grid-cols-1  md:grid-cols-4   xl:grid-cols-4 xl:max-w-6xl mx-auto'> 
           {/* Stories  */}
-          <section className="">
+          <section className="col-span-3">
               {/* <h1 className="ml-8  mr-8 font-bold text-xl">#Stories</h1> */}
               <Stories />
           </section>
